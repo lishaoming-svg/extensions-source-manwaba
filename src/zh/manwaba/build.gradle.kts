693f548a1f74e961a870ddb099aa1f0dad382ea7
lishaoming-svg <baoxialu869@gmail.com>
2026-06-16 14:01:10 +0800

    Update build.gradle.kts

--- src/zh/manwaba/build.gradle.kts
+++ src/zh/manwaba/build.gradle.kts
@@ -1,13 +1,9 @@
-plugins {
-    id("eu.kanade.tachiyomi.extension")
-}
+apply(from = "$rootDir/common.gradle")
 
 ext {
-    name = "漫蛙吧"
-    lang = "zh"
-    baseUrl = "https://www.mhtmh.org"
-    versionCode = 3
-    versionName = "1.0.3"
-    packageName = "eu.kanade.tachiyomi.extension.zh.manwaba"
-    isNsfw = true
+    set("extName", "漫蛙吧")
+    set("pkgNameSuffix", "zh.manwaba")
+    set("extClass", ".ManWaBa")
+    set("extVersionCode", 1)
+    set("isNsfw", true)
 }

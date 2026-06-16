9ccf9b031b5a9eccc4c68ded75818f4e41b3e9bc
lishaoming-svg <baoxialu869@gmail.com>
2026-06-16 14:11:58 +0800

    Update and rename build.gradle.kts to build.gradle

--- src/zh/manwaba/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-apply(from = "$rootDir/common.gradle")
-
-ext {
-    set("extName", "漫蛙吧")
-    set("pkgNameSuffix", "zh.manwaba")
-    set("extClass", ".ManWaBa")
-    set("extVersionCode", 1)
-    set("isNsfw", true)
-}
